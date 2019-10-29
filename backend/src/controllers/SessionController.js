e90f46812715ffaaeea2e7c50e805bc5118fc60c
James Amorim <jamesfsamorim@gmail.com>
2019-10-28 21:23:53 -0300

    Finished all desktop layout

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -3,7 +3,7 @@ const User = require('../models/User')
 module.exports = {
     async store (req, res) {
         const { email } = req.body
-        let user = User.findOne({ email })
+        let user = await User.findOne({ email })
 
         if(!user) {
             user = await User.create({ email })
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const path = require('path')
+const cors = require('cors')
 
 const app = express()
 
@@ -9,6 +11,8 @@ mongoose.connect("mongodb+srv://omni:omniadmin@tindevcluster-mt9dj.mongodb.net/a
     useUnifiedTopology: true
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.listen(3333)
